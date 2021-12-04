fe8181358aa8126ab35db016fc715a22d835ca1e
antipups <kurkurin2001@gmail.com>
2021-12-04 12:20:37 +0300

    Initial Commit

---
+++ NewTest4/NewTest4App.swift
@@ -0,0 +1,17 @@
+//
+//  NewTest4App.swift
+//  NewTest4
+//
+//  Created by Никита Куркурин on 04.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewTest4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
